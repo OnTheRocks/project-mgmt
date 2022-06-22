8eceaf535d3b41220b672c2fceba44614ca0ee02
OnTheRocks <nathan_huber@yahoo.com>
2022-06-22 17:20:53 -0500

    Created Project Modal.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import AddClientModal from '../components/AddClientModal'
+import AddProjectModal from '../components/AddProjectModal'
 import Clients from '../components/Clients'
 import Projects from '../components/Projects'
 
 export default function Home() {
   return (
     <>
-      <div className="dflex gap-3 mb-4">
+      <div className="d-flex gap-3 mb-4">
         <AddClientModal />
+        <AddProjectModal />
       </div>      
       <Projects />
       <hr />
