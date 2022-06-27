032ed0bf431d313c02d8103b08c851a93666f28d
OnTheRocks <nathan_huber@yahoo.com>
2022-06-27 15:48:49 -0500

    Delete Button Functionality.

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@apollo/client";
 import { Link, useParams } from "react-router-dom"
 import ClientInfo from "../components/ClientInfo";
+import DeleteProjectButton from "../components/DeleteProjectButton";
 import Spinner from "../components/Spinner";
 import { GET_PROJECT } from "../queries/projectQueries";
 
@@ -28,9 +29,7 @@ export default function Project() {
 
           <ClientInfo client={data.project.client} />
 
-
-
-          
+          <DeleteProjectButton projectId= {data.project.id} />          
         </div>
       )}
     </>
